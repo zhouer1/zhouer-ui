d0113cdb2b0f30d1fe3292cd359b38d725e265d3
zhouer1 <1528634948@qq.com>
2024-04-15 14:16:01 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import mist from '@mistjs/eslint-config'
 
 export default mist({
-    rules:{
-        "no-console": 0,
-        "no-debugger": 0,
-    }
+  rules: {
+    'no-console': 0,
+    'no-debugger': 0,
+  },
 })
